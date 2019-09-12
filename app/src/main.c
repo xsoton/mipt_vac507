034557d627468fc2a69cdb31b26e583a60b39959
Alexey Katsaba <killingrain@gmail.com>
2019-09-13 01:10:22 +0300

    Split worker into worker and writer (not tested)

--- app/src/main.c
+++ app/src/main.c
@@ -9,40 +9,77 @@
 #include "appa208.h"
 #include "qj3003p.h"
 
+#define QJ
+#define LOMO
+#define APPA
+
 #define LOMO_TTY "/dev/ttyUSB2"
 #define QJ3003P_TTY "/dev/ttyUSB0"
 #define APPA208_TTY "/dev/ttyUSB1"
+
+#define ERR(c, g, s, ...) do{if(c){fprintf(stderr,s,__VA_ARGS__);set_run(p, 0);goto g;}}while(0);
+#define ERR_PL(c, g, s, ...) do{if(c){fprintf(stderr,s,__VA_ARGS__);set_run(p, 0);data_ready_signal(p);goto g;}}while(0);
 
 typedef struct
 {
 	pthread_rwlock_t run_lock;
 	int run;
-	pthread_mutex_t plot_mutex;
-	pthread_cond_t plot_cond;
+	pthread_mutex_t eq_init_mutex;
+	pthread_cond_t eq_init_cond;
+	pthread_mutex_t data_ready_mutex;
+	pthread_cond_t data_ready_cond;
+	pthread_mutex_t data_written_mutex;
+	pthread_cond_t data_written_cond;
 	char filename[100];
+	int index;
+#ifdef QJ
+	double pps_U;
+	double pps_I;
+#endif
+#ifdef LOMO
+	double adc;
+#endif
+#ifdef APPA
+	double mult_value;
+	unit_t mult_unit;
+	int mult_overload;
+#endif
 } params_t;
-
-#define PARAMS_INIT {PTHREAD_RWLOCK_INITIALIZER, 1, PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, {0}}
 
 void *commander(void *arg);
 void *worker(void *arg);
+void *writer(void *arg);
 void *plotter(void *arg);
 
 int get_run(params_t *p);
 void set_run(params_t *p, int run);
 
-void plot_wait(params_t *p);
-void plot_signal(params_t *p);
+void eq_init_wait(params_t *p);
+void eq_init_signal(params_t *p);
+
+void data_ready_wait(params_t *p);
+void data_ready_signal(params_t *p);
+
+void data_written_wait(params_t *p);
+void data_written_signal(params_t *p);
+void data_written_broadcast(params_t *p);
 
 int main(int argc, char const *argv[])
 {
 	int ret = 0;
-	// int r;
 
-	params_t params = PARAMS_INIT;
+	params_t params = {0};
+	pthread_rwlock_init(&params.run_lock, NULL);
+	pthread_mutex_init(&params.eq_init_mutex, NULL);
+	pthread_cond_init(&params.eq_init_cond, NULL);
+	pthread_mutex_init(&params.data_ready_mutex, NULL);
+	pthread_cond_init(&params.data_ready_cond, NULL);
+	pthread_mutex_init(&params.data_written_mutex, NULL);
+	pthread_cond_init(&params.data_written_cond, NULL);
 
 	pthread_t t_commander;
 	pthread_t t_worker;
+	pthread_t t_writer;
 	pthread_t t_plotter;
 
 	if (argc < 2)
@@ -59,9 +96,11 @@ int main(int argc, char const *argv[])
 
 	pthread_create(&t_commander, NULL, commander, &params);
 	pthread_create(&t_worker, NULL, worker, &params);
+	pthread_create(&t_writer, NULL, worker, &params);
 	pthread_create(&t_plotter, NULL, plotter, &params);
 
 	pthread_join(t_worker, NULL);
+	pthread_join(t_writer, NULL);
 	pthread_join(t_plotter, NULL);
 
 	pthread_cancel(t_commander);
@@ -112,16 +151,11 @@ void *commander(void *arg)
 	return NULL;
 }
 
-#define LOMO
-#define APPA
-// #define QJ
-
 void *worker(void *arg)
 {
 	params_t *p = (params_t *)arg;
 	int r;
 
-	FILE *fp;
 #ifdef LOMO
 	int lomo_fd;
 #endif
@@ -132,46 +166,26 @@ void *worker(void *arg)
 	int qj_fd;
 #endif
 
-	int index = 0;
 	double step_U = 0.1;
 
-#ifdef LOMO
-	double adc;
-#endif
 #ifdef APPA
 	appa208_disp_t disp;
-	double mult_value;
-	unit_t mult_unit;
-	int mult_overload;
 #endif
-#ifdef QJ
-	double pps_U;
-	double pps_I;
-#endif
-
-
-#define ERR(c, g, s, ...) do{if(c){fprintf(stderr,s,__VA_ARGS__);set_run(p, 0);goto g;}}while(0);
-
-// printf("01\n");
 
 #ifdef LOMO
 	r = lomo_open(LOMO_TTY, &lomo_fd);
 		ERR(r < 0, worker_exit, "# E: Unable to open lomo (%d)", r);
-// printf("011\n");
 	r = lomo_init(lomo_fd);
 		ERR(r < 0, worker_close_lomo, "# E: Unable to init lomo (%d)", r);
 #endif
 
-// printf("02\n");
 #ifdef APPA
 	r = appa208_open(APPA208_TTY, &appa_fd);
 		ERR(r < 0, worker_close_lomo, "# E: Unable to open appa (%d)", r);
 	r = appa208_read_disp(appa_fd, &disp);
 		ERR(r < 0, worker_close_appa, "# E: Unable to read appa display (%d)", r);
-	mult_unit = appa208_get_unit(&disp.mdata);
+	p->mult_unit = appa208_get_unit(&disp.mdata);
 #endif
-
-// printf("03\n");
 
 #ifdef QJ
 	r = qj3003p_open(QJ3003P_TTY, &qj_fd);
@@ -186,116 +200,55 @@ void *worker(void *arg)
 		ERR(r < 0, worker_exit_qj, "# E: Unable to set qj output (%d)\n", r);
 #endif
 
-
-	fp = fopen(p->filename, "w+");
-		ERR(fp == NULL, worker_exit_qj, "# E: Unable to open file \"%s\"\n", p->filename);
-
-	setlinebuf(fp);
-
-	int col = 1;
-	fprintf(fp, "# %d: index\n", col++);
-#ifdef QJ
-	fprintf(fp, "# %d: pps U, V\n", col++);
-	fprintf(fp, "# %d: pps I, A\n", col++);
-#endif
-#ifdef LOMO
-	fprintf(fp, "# %d: lomo adc, a.u.\n", col++);
-#endif
-#ifdef APPA
-	fprintf(fp, "# %d: appa value, %s\n", col++, appa208_str_unit(mult_unit));
-	fprintf(fp, "# %d: appa overload, bool\n", col++);
-#endif
+	eq_init_signal(p);
 
 	while(get_run(p))
 	{
-#define ERRL(c, g, s, ...) do{if(c){fprintf(stderr,s,__VA_ARGS__);set_run(p, 0);plot_signal(p);goto g;}}while(0);
-		// if (index * step_U > 10)
-		// {
-		// 	set_run(p, 0);
-		// 	plot_signal(p);
-		// 	continue;
-		// }
+		if ((p->index + 1) * step_U > 10)
+		{
+			set_run(p, 0);
+			goto worker_end_loop;
+		}
+
+		data_written_wait(p);
+
+		p->index++;
 
 #ifdef QJ
-		r = qj3003p_set_voltage(qj_fd, index * step_U);
-			ERRL(r < 0, worker_close_file, "# E: Unable to set qj voltage (%d)\n", r);
+		r = qj3003p_set_voltage(qj_fd, p->index * step_U);
+			ERR(r < 0, worker_end_loop, "# E: Unable to set qj voltage (%d)\n", r);
+
+		sleep(1);
+
+		r = qj3003p_get_voltage(qj_fd, &p->pps_U);
+			ERR(r < 0, worker_end_loop, "# E: Unable to get qj voltage (%d)\n", r);
+		r = qj3003p_get_current(qj_fd, &p->pps_I);
+			ERR(r < 0, worker_end_loop, "# E: Unable to get qj current (%d)\n", r);
 #endif
-
-		// sleep(1);
-
-#ifdef QJ
-		r = qj3003p_get_voltage(qj_fd, &pps_U);
-			ERRL(r < 0, worker_close_file, "# E: Unable to get qj voltage (%d)\n", r);
-#endif
-
-#ifdef QJ
-		r = qj3003p_get_current(qj_fd, &pps_I);
-			ERRL(r < 0, worker_close_file, "# E: Unable to get qj current (%d)\n", r);
-#endif
-
-// printf("1\n");
 
 #ifdef LOMO
-		r = lomo_read_value(lomo_fd, &adc);
-			ERRL(r < 0, worker_close_file, "# E: Unable to read lyomo adc (%d)\n", r);
+		r = lomo_read_value(lomo_fd, &p->adc);
+			ERR(r < 0, worker_end_loop, "# E: Unable to read lomo adc (%d)\n", r);
 #endif
-
-// printf("2\n");
 
 #ifdef APPA
 		r = appa208_read_disp(appa_fd, &disp);
-			ERRL(r < 0, worker_close_file, "# E: Unable to read appa display (%d)\n", r);
-		mult_value = appa208_get_value(&disp.mdata);
-		mult_unit = appa208_get_unit(&disp.mdata);
-		mult_overload = appa208_get_overload(&disp.mdata);
+			ERR(r < 0, worker_end_loop, "# E: Unable to read appa display (%d)\n", r);
+		p->mult_value = appa208_get_value(&disp.mdata);
+		p->mult_unit = appa208_get_unit(&disp.mdata);
+		p->mult_overload = appa208_get_overload(&disp.mdata);
 #endif
 
-// printf("3\n");
+worker_end_loop:
 
-		fprintf(fp,
-			"%d"
-#ifdef QJ
-			"\t%le"
-			"\t%le"
-#endif
-#ifdef LOMO
-			"\t%le"
-#endif
-#ifdef APPA
-			"\t%le"
-			"\t%d"
-#endif
-			"\n"
-			, index
-#ifdef QJ
-			, pps_U
-			, pps_I
-#endif
-#ifdef LOMO
-			, adc
-#endif
-#ifdef APPA
-			, mult_value
-			, mult_overload
-#endif
-		);
-
-		// index++;
-
-		plot_signal(p);
+		data_ready_signal(p);
 	}
-
-	plot_signal(p);
-
-worker_close_file:
-
-	fclose(fp);
 
 worker_exit_qj:
 
 #ifdef QJ
 	r = qj3003p_set_voltage(qj_fd, 0);
-		ERRL(r < 0, worker_close_qj, "# E: Unable to set qj voltage (%d)\n", r);
+		ERR(r < 0, worker_close_qj, "# E: Unable to set qj voltage (%d)\n", r);
 	r = qj3003p_set_output(qj_fd, 0);
 		ERR(r < 0, worker_close_qj, "# E: Unable to set qj voltage (%d)\n", r);
 #endif
@@ -326,6 +279,90 @@ worker_exit:
 	return NULL;
 }
 
+void *writer(void *arg)
+{
+	params_t *p = (params_t *)arg;
+	int r;
+
+	FILE *fp;
+
+	fp = fopen(p->filename, "w+");
+		ERR(fp == NULL, writer_exit, "# E: Unable to open file \"%s\" (%s)\n", p->filename, strerror(ferror(fp)));
+
+	setlinebuf(fp);
+
+	eq_init_wait(p);
+
+	int col = 1;
+	r = fprintf(fp, "# %d: index\n", col++);
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+#ifdef QJ
+	r = fprintf(fp, "# %d: pps U, V\n", col++);
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+	r = fprintf(fp, "# %d: pps I, A\n", col++);
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+#endif
+#ifdef LOMO
+	r = fprintf(fp, "# %d: lomo adc, a.u.\n", col++);
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+#endif
+#ifdef APPA
+	r = fprintf(fp, "# %d: appa value, %s\n", col++, appa208_str_unit(p->mult_unit));
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+	r = fprintf(fp, "# %d: appa overload, bool\n", col++);
+		ERR(r < 0, writer_close, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+#endif
+
+	while(get_run(p))
+	{
+		data_ready_wait(p);
+
+		r = fprintf(fp,
+			"%d"
+#ifdef QJ
+			"\t%le"
+			"\t%le"
+#endif
+#ifdef LOMO
+			"\t%le"
+#endif
+#ifdef APPA
+			"\t%le"
+			"\t%d"
+#endif
+			"\n"
+			, p->index
+#ifdef QJ
+			, p->pps_U
+			, p->pps_I
+#endif
+#ifdef LOMO
+			, p->adc
+#endif
+#ifdef APPA
+			, p->mult_value
+			, p->mult_overload
+#endif
+		);
+
+		if(r < 0)
+		{
+			fprintf(stderr, "# E: Unable to print to file \"%s\" (%s)\n", p->filename, strerror(r));
+			set_run(p, 0);
+		}
+
+		data_written_broadcast(p);
+	}
+
+writer_close:
+
+	fclose(fp);
+
+writer_exit:
+
+	return NULL;
+}
+
 void *plotter(void *arg)
 {
 	params_t *p = (params_t *)arg;
@@ -351,7 +388,7 @@ void *plotter(void *arg)
 
 	while(get_run(p))
 	{
-		plot_wait(p);
+		data_written_wait(p);
 
 		fprintf(gp, "plot \"%s\" u 3:2 w l\n", p->filename);
 	}
@@ -359,6 +396,7 @@ void *plotter(void *arg)
 	pclose(gp);
 
 plotter_exit:
+
 	return NULL;
 }
 
@@ -378,16 +416,51 @@ void set_run(params_t *p, int run)
 	pthread_rwlock_unlock(&p->run_lock);
 }
 
-void plot_wait(params_t *p)
+void eq_init_wait(params_t *p)
 {
-	pthread_mutex_lock(&p->plot_mutex);
-		pthread_cond_wait(&p->plot_cond, &p->plot_mutex);
-	pthread_mutex_unlock(&p->plot_mutex);
+	pthread_mutex_lock(&p->eq_init_mutex);
+		pthread_cond_wait(&p->eq_init_cond, &p->eq_init_mutex);
+	pthread_mutex_unlock(&p->eq_init_mutex);
 }
 
-void plot_signal(params_t *p)
+void eq_init_signal(params_t *p)
 {
-	pthread_mutex_lock(&p->plot_mutex);
-		pthread_cond_signal(&p->plot_cond);
-	pthread_mutex_unlock(&p->plot_mutex);
+	pthread_mutex_lock(&p->eq_init_mutex);
+		pthread_cond_signal(&p->eq_init_cond);
+	pthread_mutex_unlock(&p->eq_init_mutex);
+}
+
+void data_ready_wait(params_t *p)
+{
+	pthread_mutex_lock(&p->data_ready_mutex);
+		pthread_cond_wait(&p->data_ready_cond, &p->data_ready_mutex);
+	pthread_mutex_unlock(&p->data_ready_mutex);
+}
+
+void data_ready_signal(params_t *p)
+{
+	pthread_mutex_lock(&p->data_ready_mutex);
+		pthread_cond_signal(&p->data_ready_cond);
+	pthread_mutex_unlock(&p->data_ready_mutex);
+}
+
+void data_written_wait(params_t *p)
+{
+	pthread_mutex_lock(&p->data_written_mutex);
+		pthread_cond_wait(&p->data_written_cond, &p->data_written_mutex);
+	pthread_mutex_unlock(&p->data_written_mutex);
+}
+
+void data_written_signal(params_t *p)
+{
+	pthread_mutex_lock(&p->data_written_mutex);
+		pthread_cond_signal(&p->data_written_cond);
+	pthread_mutex_unlock(&p->data_written_mutex);
+}
+
+void data_written_broadcast(params_t *p)
+{
+	pthread_mutex_lock(&p->data_written_mutex);
+		pthread_cond_broadcast(&p->data_written_cond);
+	pthread_mutex_unlock(&p->data_written_mutex);
 }
